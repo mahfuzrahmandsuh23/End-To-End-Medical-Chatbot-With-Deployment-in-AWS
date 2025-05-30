a4893442d18402bac3dba29d04fa511f8c0b27ce
Muhammad Mahfuzur Rahman <mahfuzur993.rahman@gmail.com>
2025-05-30 12:10:08 +0100

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Mahfuz Rahman',
+    author_email= 'mahfuzur993.rahman@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
